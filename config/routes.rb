2745b1bc4c8c9eb1f8477266bfc0cb08deaaac4d
Keisuke Mizoguchi <mizoguchikeisuke@mizoguchikeisuke-no-MacBook-Pro.local>
2017-04-10 15:36:23 +0900

    修正2
    ・routesの設定
    ・不要なファイルの削除コマンド

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'messages' => 'messages#index'
+devise_for :users
+  root 'messages#index'
+  resources :groups, only: [:new, :edit, :create] do
+    resources :messages, only: [:index]
+  end
 end
