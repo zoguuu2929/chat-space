1064231710613ba98a4eecf6724ce6e70d61b4cd
Keisuke Mizoguchi <mizoguchikeisuke@mizoguchikeisuke-no-MacBook-Pro.local>
2017-04-20 19:49:11 +0900

    サインアップ準備
    ・mixinの追加
    ・scssの記載
    ・フォルダ名変更に伴うimport名変更
    ・edit,new画面の作成
    ・indexへのlink_to追加
    ・重複ファイルの削除
    ・マイグレーションファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
   resources :groups, only: [:new, :edit, :create] do
     resources :messages, only: [:index]
