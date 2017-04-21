2ca0c06bcdf2cdebfa394d7d25e6fef4cb8b6880
Keisuke Mizoguchi <mizoguchikeisuke@mizoguchikeisuke-no-MacBook-Pro.local>
2017-04-21 20:04:19 +0900

    インデックスの追加、サインアップの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420103640) do
+ActiveRecord::Schema.define(version: 20170421083839) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170420103640) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
