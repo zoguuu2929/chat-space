b33e6c9e4669bba8eadc7f98e96233ea9d8cceba
Keisuke Mizoguchi <mizoguchikeisuke@mizoguchikeisuke-no-MacBook-Pro.local>
2017-05-04 11:02:22 +0900

    コントローラテスト

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,6 +1,45 @@
 require 'rails_helper'
 
-describe MessagesController do
+RSpec.describe MessagesController, type: :controller do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
 
+  context 'ログインなし' do
+    describe 'GET #index' do
+      it 'returns http success' do
+        get :index, group_id: group.id
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    describe 'GET #create' do
+      it 'returns http success' do
+        get :create, group_id: group.id, message: { body: 'サンプル' }
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  context 'ログイン中' do
+    before do
+      login_user(user)
+    end
+
+    describe 'GET #index' do
+      it 'returns http success' do
+        get :index, group_id: group.id
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET #create' do
+      it 'returns http success' do
+        get :create, group_id: group.id, message: { text: 'メッセージ内容' }
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(group_messages_path(group))
+      end
+    end
+  end
 end
-
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :message do
-    text "hello!"
-    image "hoge.png"
-    user_id 1
-    created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
+    body 'メッセージサンプル'
+    chat_group
+    user
   end
 end
